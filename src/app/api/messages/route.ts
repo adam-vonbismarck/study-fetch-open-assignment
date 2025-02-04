d6fe32144bd84a5a3976a92e46361f581ee48560
adam-vonbismarck <adam_von_bismarck@brown.edu>
2025-02-03 20:20:17 -0500

    Changing OpenAI model for speed

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -137,7 +137,7 @@ export async function POST(req: Request) {
         }
 
         const response = await openai.chat.completions.create({
-          model: "gpt-4o-mini",
+          model: "gpt-4o",
           messages: augmentedMessages,
         });
         return NextResponse.json({result: response.choices[0].message});
